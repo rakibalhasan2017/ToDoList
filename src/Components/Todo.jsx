e5d0b98f89e7431d70c1df57a9462615dae13414
rakib <rakuhasan2017@gmai.com>
2024-11-01 23:38:19 +0600

    take Todo Items as an input and the save the item hitting button.

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react"
+import TodoItem from "./TodoItem";
 
 export default function() {
     const [item, seTitem] = useState("");
+    const [todos, seTtodos] = useState([]);
     function handleclick(event) {
         event.preventDefault();
+        seTtodos([...todos, item]);
+        seTitem("");
     }
     return (
         <div>
@@ -18,6 +22,12 @@ export default function() {
                   Add me
                  </button>
             </form>
+            <ol>
+            {todos.map((todo) => (
+              <TodoItem key={todo} todo = {todo}/>
+            ))}
+            </ol>
+            
         </div>
     )
 }
---
+++ src/Components/TodoItem.jsx
@@ -0,0 +1,5 @@
+export default function({todo}) {
+    return (
+        <h3><li>{todo}</li></h3>
+    )
+}
